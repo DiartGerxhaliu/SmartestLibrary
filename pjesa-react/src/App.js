14fbec795dbcd592c6a8397062f48e925a311330
DiartGerxhaliu <diart.gerxhaliu@student.uni-pr.edu>
2026-01-03 15:56:13 +0100

    rregullimi i footerit ne app.js

--- pjesa-react/src/App.js
+++ pjesa-react/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/templates/Home.jsx'
 import Menu from './components/Organisms/Menu';
-
+import Footer from './components/Organisms/footer';
 function App() {
   return (
     <div className="App">
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
-        
+        <Footer />
       </BrowserRouter>
     </div>
   );
